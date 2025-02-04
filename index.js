823fd6cfeb698be8bab5190148377bbbde6d41f0
Julian Fong <103627958+gragbag@users.noreply.github.com>
2025-02-03 23:19:34 -0800

    Deployment commit

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const SetupController_1 = require("./SetupController");
-require("./styles.css");
-(0, SetupController_1.startSetup)();
